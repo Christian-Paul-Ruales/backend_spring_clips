501c9a22fe3d470eb915d4a98ad19a0d59a13cb1
Christian-Paul-Ruales <christianpaulruales@gmail.com>
2019-11-20 13:12:57 -0500

    preguntas

--- src/main/java/com/expertos/comidas/controller/comidasController.java
+++ src/main/java/com/expertos/comidas/controller/comidasController.java
@@ -44,56 +44,145 @@ public class comidasController {
 			/**Lista*/
 		System.out.println(" ----------------------Que me llego ---"+r.getResponse());
 		String [] llegadas =r.getResponse().split("-");
-		CLIPS clip=new CLIPS("primera.CLP");
-		r.setResponse(llegadas[0]);
-		System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+		int []primera= {0,0,0,0,0,0,0};
 
-		int []primera=clip.evaluate(r,0);
+		try {
+			CLIPS clip=new CLIPS("primera.CLP");
+			r.setResponse(llegadas[0]);
+			System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+			
+			primera=clip.evaluate(r,0);
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			
+			System.out.print("----------------------------- Fallo primera");
+		}
 		
-		CLIPS clip2=new CLIPS("segunda.CLP");
-		r.setResponse(llegadas[1]);
-		System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+		int []segunda= {0,0,0,0,0,0,0};
+		try {
+			CLIPS clip2=new CLIPS("segunda.CLP");
+			r.setResponse(llegadas[1]);
+			System.out.println(" ----------------------Que me llego ---"+r.getResponse());
 
-		int []segunda=clip2.evaluate(r,2);
-		
-		CLIPS clip3=new CLIPS("tercera.CLP");
-		r.setResponse(llegadas[2]);
-		System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+			segunda=clip2.evaluate(r,2);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.print("----------------------------- Fallo segunda");
 
-		int []tercera=clip3.evaluate(r,3);
+		}
 		
-		CLIPS clip4=new CLIPS("cuarto.CLP");
-		r.setResponse(llegadas[3]);
-		System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+		int []tercera= {0,0,0,0,0,0,0};
+		try {
+			CLIPS clip3=new CLIPS("tercera.CLP");
+			r.setResponse(llegadas[2]);
+			System.out.println(" ----------------------Que me llego ---"+r.getResponse());
 
-		int []cuarta=clip4.evaluate(r,4);
-		
-		CLIPS clip5=new CLIPS("quinto.CLP");
-		r.setResponse(llegadas[4]);
-		System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+			tercera=clip3.evaluate(r,3);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.print("----------------------------- Fallo tercera");
 
-		int []quinta=clip5.evaluate(r,5);
-		
-		
-		CLIPS clip6=new CLIPS("sexto.CLP");
-		r.setResponse(llegadas[5]);
-		System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+		}
+		int []cuarta= {0,0,0,0,0,0,0};
+		try {
+			CLIPS clip4=new CLIPS("cuarto.CLP");
+			r.setResponse(llegadas[3]);
+			System.out.println(" ----------------------Que me llego ---"+r.getResponse());
 
-		int []sexta=clip6.evaluate(r,6);
-		
-		CLIPS clip7=new CLIPS("septimo.CLP");
-		r.setResponse(llegadas[6]);
-		System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+			cuarta=clip4.evaluate(r,4);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.print("----------------------------- Fallo cuarta");
 
-		int []septima=clip7.evaluate(r,7);
+		}
+		
+		int [] quinta= {0,0,0,0,0,0};
+		try {
+			CLIPS clip5=new CLIPS("quinto.CLP");
+			r.setResponse(llegadas[4]);
+			System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+
+			quinta=clip5.evaluate(r,5);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.print("----------------------------- Fallo quinta");
+
+		}
+		
+		int []sexta= {0,0,0,0,0,0};
+		try {
+			CLIPS clip6=new CLIPS("sexto.CLP");
+			r.setResponse(llegadas[5]);
+			System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+
+			sexta=clip6.evaluate(r,6);
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.print("----------------------------- Fallo sexta");
+
+		}
+		int []septima= {0,0,0,0,0,0,0};
+		try {
+			CLIPS clip7=new CLIPS("septimo.CLP");
+			r.setResponse(llegadas[6]);
+			System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+
+			septima=clip7.evaluate(r,7);
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.print("----------------------------- Fallo septima");
+
+		}
+		
+		int []octava= {0,0,0,0,0,0,0};
+		try {
+			CLIPS clip8=new CLIPS("octava.CLP");
+			r.setResponse(llegadas[7]);
+			System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+
+			octava=clip8.evaluate(r,8);
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.print("----------------------------- Fallo octava");
+
+		}
+	
+		int []novena= {0,0,0,0,0,0,0};
+		try {
+			CLIPS clip9=new CLIPS("novena.CLP");
+			r.setResponse(llegadas[8]);
+			System.out.println(" ----------------------Que me llego ---"+r.getResponse());
+
+			novena=clip9.evaluate(r,8);
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.print("----------------------------- Fallo novena");
+
+		}
+		
 		
 		int []total=new int[7];
 		for (int i = 0; i < total.length; i++) {
-			total[i]=primera[i]+segunda[i]+tercera[i]+cuarta[i]+quinta[i]+sexta[i]+septima[i];
+			total[i]=primera[i]+segunda[i]+tercera[i]+cuarta[i]+quinta[i]+sexta[i]+septima[i]
+					+octava[i]+novena[i];
 		}
 			//results res=new results();
 			String resp=total[0]+"-"+total[1]+"-"+total[2]+"-"+total[3]
-					+"-"+total[4]+"-"+total[5]+"-"+total[6];
+					+"-"+total[4]+"-"+total[5]+"-"+total[6] ;
 			results res=new results(resp);
 			System.out.println("----------------------Que envio ?:"+res.getResponse());
 
